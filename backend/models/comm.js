828ff3dea2be25e1e16c779fb7f3bd4c0766ade4
Ruben Caceres Jr <rcaceres.jr@gmail.com>
2019-10-04 17:47:41 -0500

    adds initial routes and models; connects to mongodb server

---
+++ backend/models/comm.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose'); 
+
+const Schema = mongoose.Schema;
+
+const commSchema = new Schema({
+    username: { type: String, required: true },
+    student: { type: String, required: true }, 
+    date: { type: Date, required: true },
+}, {
+    timestamps: true,
+});
+
+const Comm = mongoose.model('Comm', commSchema);
+
+module.exports = Comm;
