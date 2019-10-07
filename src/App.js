343712a14c584b4e0277e6bdbf12b6d60134ddaf
Ruben Caceres Jr <rcaceres.jr@gmail.com>
2019-10-07 15:39:36 -0500

    bcreates a baby-stepp app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import CommsList from "./components/comms-list.jsx";
+import EditComm from "./components/edit-comm.jsx";
+import CreateComm from "./components/create-comm.jsx";
+import CreateUser from "./components/create-user.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Khas App Under Construction</h1>
-      <p>Check back soon!</p>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Route path="/" exact component={CommsList} />
+        <Route path="/edit/:id" component={EditComm} />
+        <Route path="/create" component={CreateComm} />
+        <Route path="/user" component={CreateUser} />
+        <br />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/edit-comm.jsx
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+class EditComm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeStudent = this.onChangeStudent.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      student: "",
+      date: new Date(),
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/comms/" + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          student: response.data.student,
+          date: new Date(response.data.date)
+        }).catch(function(error) {
+          console.log(error);
+        });
+      });
+
+    axios
+      .get("http://localhost:5000/users/")
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username)
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeStudent(e) {
+    this.setState({
+      student: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const comm = {
+      username: this.state.username,
+      student: this.state.student,
+      date: this.state.date
+    };
+    console.log(comm);
+
+    axios
+      .post(
+        "http://localhost:5000/comms/update" + this.props.match.params.id,
+        comm
+      )
+      .then(res => console.log(res.data));
+
+    window.location = "/";
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Edit Communication Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Student: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.student}
+              onChange={this.onChangeStudent}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Communication Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EditComm;
